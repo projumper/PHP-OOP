a49d37f3edabfb9c5018ec8f49256b5a5c55ecb8
Your Name <you@example.com>
2024-04-08 10:56:16 +0200

    kommentar hinzugefügt

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$variable = "dsf";
+$variable = 12;
+
+for ($i=0; $i < 11; $i++) { 
+    # code...
+}
+
+foreach ($variable as $key => $value) {
+    # code...
+}
+
+while ($b <= 10) {
+    # code...
+}
+
+/*das ist ein Kommentar */
+/*das ist ein Kommentar */
